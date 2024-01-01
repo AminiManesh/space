3e7509045a32672c0621275b02d72aadef04f6de
AminiManesh <aztecgoodamin1@gmail.com>
2024-01-01 22:35:30 +0330

    Add admin.html, Update Index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 $(window).on('load', function () {
-    
+
     $('.counter').counter({
         autoStart: true,
         duration: 1500,
@@ -24,7 +24,7 @@ $(window).on('load', function () {
 });
 
 $(window).on('scroll', function () {
-    
+
     navbarCheck();
     $('.anim').each(function (index, element) {
         if (isInViewport(this)) {
@@ -38,10 +38,10 @@ $(window).on('scroll', function () {
 });
 
 $(window).on('resize', function () {
-    
+
 });
 
-$(document).ready(function () {
+$(document).ready(async function () {
 
     particlesJS.load('particles-js', 'assets/particles/particles.json', function () {
         console.log('callback - particles.js config loaded');
@@ -57,6 +57,11 @@ $(document).ready(function () {
         // options
     });
 
+    $('input[type="checkbox"]').on('change', function () {
+        addSelectedFilter($(this).attr("id"));
+    });
+
+    await delay(1500);
     new WOW().init();
 });
 
@@ -76,4 +81,82 @@ function isInViewport(element) {
         rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
+}
+
+function showFilter(e, mode) {
+
+    var target = $(e).parent().parent().parent().find('.filter-section-which');
+
+    switch (mode) {
+        case "no":
+            $(target).slideUp(300);
+            $(target.find('input')).each(function (index, element) {
+                this.checked = false;
+                var event = new Event('change');
+                this.dispatchEvent(event);
+            });
+            break;
+
+        case "has":
+            $(target).slideDown(300);
+            break;
+
+        case "all":
+            $(target).slideDown(300);
+            $(target.find('input')).each(function (index, element) {
+                this.checked = true;
+                var event = new Event('change');
+                this.dispatchEvent(event);
+            });
+            break;
+
+        default:
+            break;
+    }
+}
+
+// an async function for set delay
+function delay(milliseconds) {
+    return new Promise(resolve => {
+        setTimeout(resolve, milliseconds);
+    });
+}
+
+function addSelectedFilter(id) {
+    var checked = document.getElementById(id).checked;
+    var text = $('#' + id).parent().find('label').text();
+
+    if (checked) {
+        var selectedFilter = document.createElement('span');
+        $(selectedFilter).addClass('selected-filter');
+
+        var selectedFilterText = document.createElement('span');
+        $(selectedFilterText).addClass('selected-filter-text');
+
+        var selectedFilterIcon = document.createElement('span');
+        $(selectedFilterIcon).addClass('bi');
+        $(selectedFilterIcon).addClass('bi-x');
+        $(selectedFilterIcon).addClass('vertical-center');
+
+        $(selectedFilter).append(selectedFilterText);
+        $(selectedFilter).append(selectedFilterIcon);
+
+        $(selectedFilterText).text(text + " ");
+        $(selectedFilter).attr("data-connect", id);
+        selectedFilter.addEventListener('click', function () {
+            var dataConnect = $(this).attr('data-connect');
+            removeSelectedFilter(dataConnect);
+        });
+        $('.selected-filters').append(selectedFilter);
+
+    } else {
+        $('.selected-filter[data-connect="' + id + '"]').remove();
+    }
+}
+
+function removeSelectedFilter(dataConnect) {
+    console.log(dataConnect)
+    document.getElementById(dataConnect).checked = false;
+    var event = new Event('change');
+    document.getElementById(dataConnect).dispatchEvent(event);
 }

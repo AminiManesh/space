7606eaedc1feaa93135bd6c41a0fbafd5be27326
AminiManesh <aztecgoodamin1@gmail.com>
2024-01-11 11:37:49 +0330

    Add search in filters

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,6 +41,9 @@ $(window).on('resize', function () {
 
 });
 
+
+var allFilters = $('input[type="checkbox"]');
+
 $(document).ready(async function () {
 
     particlesJS.load('particles-js', 'assets/particles/particles.json', function () {
@@ -59,6 +62,29 @@ $(document).ready(async function () {
 
     $('input[type="checkbox"]').on('change', function () {
         addSelectedFilter($(this).attr("id"));
+    });
+
+
+    $.each(allFilters, function (indexInArray, valueOfElement) {
+        var searchItem = document.createElement('li');
+        $(searchItem).addClass('search-in-filters-dropdown-list-item');
+        $(searchItem).text($(this).parent().find('label').text());
+        $(searchItem).attr('for', this.id);
+        $('.search-in-filters-dropdown-list').append(searchItem);
+    });
+
+    $('.search-in-filters-dropdown-list-item').click(function (e) {
+        e.preventDefault();
+        var id = $(this).attr('for');
+        var checkbox = document.getElementById(id);
+        checkbox.checked = true
+        var event = new Event('change');
+        checkbox.dispatchEvent(event);
+
+        var search = $('input.search-in-filters')[0];
+        $(search).val('');
+        var event2 = new Event('keyup');
+        search.dispatchEvent(event2);
     });
 
     await delay(1500);
@@ -127,38 +153,64 @@ function delay(milliseconds) {
 function addSelectedFilter(id) {
     var checked = document.getElementById(id).checked;
     var text = $('#' + id).parent().find('label').text();
+    var name = document.getElementById(id).name;
 
     if (checked) {
-        var selectedFilter = document.createElement('span');
-        $(selectedFilter).addClass('selected-filter');
+        var notLikeThis = $('.selected-filter[data-connect="' + id + '"]')
+        console.log(notLikeThis.length)
+        if (notLikeThis.length === 0) {
+            var selectedFilter = document.createElement('span');
+            $(selectedFilter).addClass('selected-filter');
+            $(selectedFilter).addClass(name);
 
-        var selectedFilterText = document.createElement('span');
-        $(selectedFilterText).addClass('selected-filter-text');
+            var selectedFilterText = document.createElement('span');
+            $(selectedFilterText).addClass('selected-filter-text');
 
-        var selectedFilterIcon = document.createElement('span');
-        $(selectedFilterIcon).addClass('bi');
-        $(selectedFilterIcon).addClass('bi-x');
-        $(selectedFilterIcon).addClass('vertical-center');
+            var selectedFilterIcon = document.createElement('span');
+            $(selectedFilterIcon).addClass('bi');
+            $(selectedFilterIcon).addClass('bi-x');
+            $(selectedFilterIcon).addClass('vertical-center');
 
-        $(selectedFilter).append(selectedFilterText);
-        $(selectedFilter).append(selectedFilterIcon);
+            $(selectedFilter).append(selectedFilterText);
+            $(selectedFilter).append(selectedFilterIcon);
 
-        $(selectedFilterText).text(text + " ");
-        $(selectedFilter).attr("data-connect", id);
-        selectedFilter.addEventListener('click', function () {
-            var dataConnect = $(this).attr('data-connect');
-            removeSelectedFilter(dataConnect);
-        });
-        $('.selected-filters').append(selectedFilter);
+            $(selectedFilterText).text(text + " ");
+            $(selectedFilter).attr("data-connect", id);
+            selectedFilter.addEventListener('click', function () {
+                var dataConnect = $(this).attr('data-connect');
+                removeSelectedFilter(dataConnect);
+            });
 
+            $('.selected-filters').prepend(selectedFilter);
+        }
     } else {
         $('.selected-filter[data-connect="' + id + '"]').remove();
     }
 }
 
 function removeSelectedFilter(dataConnect) {
-    console.log(dataConnect)
     document.getElementById(dataConnect).checked = false;
     var event = new Event('change');
     document.getElementById(dataConnect).dispatchEvent(event);
+}
+
+function searchInFilters(inputValue) {
+
+    if (inputValue) {
+        $('.search-in-filters-dropdown').fadeIn(300);
+    } else {
+        $('.search-in-filters-dropdown').fadeOut(300);
+    }
+
+    filter = inputValue.toUpperCase();
+    var searchItems = document.getElementsByClassName('search-in-filters-dropdown-list-item');
+
+    for (i = 0; i < searchItems.length; i++) {
+        txtValue = $(searchItems[i]).text();
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            searchItems[i].style.display = "";
+        } else {
+            searchItems[i].style.display = "none";
+        }
+    }
 }
